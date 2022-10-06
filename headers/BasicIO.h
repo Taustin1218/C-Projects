a48d37e114aa012ea7c8c364a61849bc0a440d15
Thomas Austin <austint@chc.edu>
2022-10-06 10:48:37 -0400

    Oct 6

---
+++ headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Thomas"
+#define LAST_NAME "Austin"
+#define WEIGHT 165.32
+#define AGE 20
+#include <stdio.h>
+
+// include function declarations 
+void output(void);
+void input(void);
---
+++ headers/Decisions.h
@@ -0,0 +1,11 @@
+// include pre-processor directives
+#define NUM 34568.80
+#include <stdbool.h>
+
+// include function declarations
+void decisions1(void);
+void decisions2(void);
+void decisions3(void);
+void decisions4(void);
+void decisions5(void);
+void decisions6(void);
